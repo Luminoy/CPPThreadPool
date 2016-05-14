82d5d36f69ae93371e2d2578197c091d7ae14fab
kobe0824 <piaoyelm@163.com>
2016-05-14 23:09:28 +0800

    C++线程池实现（70%）

--- CPPThreadPool/thread_pool_main.cpp
+++ CPPThreadPool/thread_pool_main.cpp
@@ -21,8 +21,57 @@ class ThreadPool
 {
 public:
 	//ThreadPool();
-	ThreadPool(DWORD dwNum = 4);
-	~ThreadPool();
+	ThreadPool(DWORD dwNum = 4) : _lThreadNum(0),  _lRunningNum(0) 
+	{
+		InitializeCriticalSection(&_csThreadVector);
+		InitializeCriticalSection(&_csWorkQueue);
+		_EventComplete = CreateEvent(0, false, false, NULL);
+		_EventEnd = CreateEvent(0, true, false, NULL);
+		_SemaphoreCall = CreateSemaphore(0, 0, 0x7FFFFFFF, NULL);
+		_SemaphoreDel = CreateSemaphore(0, 0, 0x7FFFFFFF, NULL);
+		assert(_SemaphoreCall != INVALID_HANDLE_VALUE);
+		assert(_SemaphoreDel != INVALID_HANDLE_VALUE);
+		assert(_EventComplete != INVALID_HANDLE_VALUE);
+		assert(_EventEnd != INVALID_HANDLE_VALUE);
+		AdjustSize(dwNum <= 0 ? 4 : dwNum);
+	}
+
+	//调整线程池规模
+	int AdjustSize(int iNum) {
+		if (iNum > 0) {
+			ThreadItem *pNew;
+			EnterCriticalSection(&_csThreadVector);
+			for (int i = 0; i < iNum; i++) {
+				_ThreadVector.push_back(pNew = new ThreadItem(this));
+				assert(pNew);
+				pNew->_Handle = CreateThread(NULL, 0, DefaultJobProc, pNew, 0, NULL);
+
+				SetThreadPriority(pNew->_Handle, THREAD_PRIORITY_BELOW_NORMAL);
+				assert(pNew->_Handle);
+			}
+			LeaveCriticalSection(&_csThreadVector);
+		}
+		else
+		{
+			iNum *= -1;
+			ReleaseSemaphore(_SemaphoreDel, iNum > _lThreadNum ? _lThreadNum : iNum, NULL);
+		}
+		return (int)_lThreadNum;
+	}
+
+	~ThreadPool() {
+		DeleteCriticalSection(&_csWorkQueue);
+		CloseHandle(_EventComplete);
+		CloseHandle(_EventEnd);
+		CloseHandle(_SemaphoreCall);
+		CloseHandle(_SemaphoreDel);
+
+		vector<ThreadItem *>::iterator iter;
+		for (iter = _ThreadVector.begin(); iter != _ThreadVector.end(); iter++) {
+			if (*iter)	delete *iter;
+		}
+		DeleteCriticalSection(&_csThreadVector);
+	}
 	//工作线程
 	static DWORD WINAPI DefaultJobProc(LPVOID lpParam = NULL) {
 		ThreadItem *pThread = static_cast<ThreadItem *>(lpParam);
@@ -65,6 +114,36 @@ public:
 				InterlockedDecrement(&pThreadPoolObj->_lRunningNum);
 			}
 		}
+		EnterCriticalSection(&pThreadPoolObj->_csThreadVector);
+		pThreadPoolObj->_ThreadVector.erase(find(pThreadPoolObj->_ThreadVector.begin(), pThreadPoolObj->_ThreadVector.end(), pThread));
+		LeaveCriticalSection(&pThreadPoolObj->_csThreadVector);
+		delete pThread;
+		InterlockedDecrement(&pThreadPoolObj->_lThreadNum);
+		if (!pThreadPoolObj->_lThreadNum) {
+			SetEvent(pThreadPoolObj->_EventComplete);
+		}
+		return 0;
+	}
+	//调用线程池
+	void Call(void(*pFunc)(void *), void *pParam = NULL) {
+		assert(pFunc);
+		EnterCriticalSection(&_csWorkQueue);
+		_JobQueue.push(new JobItem(pFunc, pParam));
+		LeaveCriticalSection(&_csWorkQueue);
+		ReleaseSemaphore(_SemaphoreCall, 1, NULL);
+	}
+	//调用线程池
+	inline void Call(ThreadJob *p, void *pParam = NULL) {
+		Call(CallProc, new CallProcPara(p, pParam));
+	}
+	inline void End() {
+		SetEvent(_EventEnd);
+	}
+	inline DWORD Size() {
+		return (DWORD)_lThreadNum;
+	}
+	inline DWORD GetRunningSize() {
+		return (DWORD)_lRunningNum;
 	}
 	//调用用户对象虚函数
 	static void CallProc(void *pData) { 
@@ -108,12 +187,6 @@ private:
 //ThreadPool::ThreadPool()
 //{
 //}
-
-ThreadPool::ThreadPool(DWORD dwNum)
-{
-	_lThreadNum = _lRunningNum = 0;
-
-}
 
 ThreadPool::~ThreadPool()
 {
